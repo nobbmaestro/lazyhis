38261a7255a018253188baf5dd3a34e2a5633297
Norbert Batiuk <norbert@batiuk.se>
2025-02-17 23:36:11 +0100

    Add CLI option for printing out default config

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,45 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
 
+	"github.com/nobbmaestro/lazyhis/pkg/config"
+	"gopkg.in/yaml.v3"
+
 	"github.com/spf13/cobra"
+)
+
+var (
+	printDefaultConfig bool
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "lazyhis",
 	Short: "lazyhis",
+	RunE:  runRoot,
+}
+
+func runRoot(cmd *cobra.Command, args []string) error {
+	switch {
+	case printDefaultConfig:
+		return runPrintDefaultConfig()
+	default:
+		return nil
+	}
+}
+
+func runPrintDefaultConfig() error {
+	var buf bytes.Buffer
+	encoder := yaml.NewEncoder(&buf)
+	err := encoder.Encode(config.GetDefaultUserConfig())
+	if err != nil {
+		return fmt.Errorf("Failed to encode default config: %w", err)
+	}
+	fmt.Printf("%s\n", buf.String())
+	return nil
 }
 
 func SetVersionInfo(version, commit, date string) {
@@ -32,4 +61,10 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.
+		Flags().
+		BoolVarP(&printDefaultConfig, "config", "c", false, "print the default config")
 }
