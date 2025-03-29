29cf1f984dd58787d429b3b9846baffe6572606b
Norbert Batiuk <norbert@batiuk.se>
2025-03-29 01:16:41 +0100

    Add hisquery widget

--- pkg/gui/model.go
+++ pkg/gui/model.go
@@ -2,12 +2,12 @@ package gui
 
 import (
 	"github.com/charmbracelet/bubbles/table"
-	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/nobbmaestro/lazyhis/pkg/config"
 	"github.com/nobbmaestro/lazyhis/pkg/domain/model"
 	"github.com/nobbmaestro/lazyhis/pkg/gui/formatters"
 	"github.com/nobbmaestro/lazyhis/pkg/gui/widgets/help"
+	"github.com/nobbmaestro/lazyhis/pkg/gui/widgets/hisquery"
 	"github.com/nobbmaestro/lazyhis/pkg/gui/widgets/histable"
 )
 
@@ -19,7 +19,7 @@ type Model struct {
 	columns           []config.Column
 	records           []model.History
 	table             histable.Model
-	input             textinput.Model
+	input             hisquery.Model
 	help              help.Model
 	height            int
 	width             int
@@ -39,11 +39,12 @@ func NewModel(
 	version string,
 	searchKeywords string,
 ) Model {
-	input := textinput.New()
-	input.SetValue(searchKeywords)
-	input.Focus()
-
 	records := queryHistory([]string{}, cfg.InitialFilterMode)
+
+	historyQuery := hisquery.New(
+		hisquery.WithValue(searchKeywords),
+		hisquery.WithFocus(),
+	)
 
 	content := formatters.NewHistoryTableContent(records, cfg.ColumnLayout, 100)
 	historyTable := histable.New(
@@ -63,7 +64,7 @@ func NewModel(
 		filterModes:       cfg.CyclicFilterModes,
 		records:           records,
 		table:             historyTable,
-		input:             input,
+		input:             historyQuery,
 		help:              help,
 		height:            10,
 		width:             10,
