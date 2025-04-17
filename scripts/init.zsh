a7f3c76d300151374c778f35d923cdb8b2d571ef
Norbert Batiuk <norbert@batiuk.se>
2025-04-17 11:15:54 +0200

    Add header to init script and provide awknowledgement

--- scripts/init.zsh
+++ scripts/init.zsh
@@ -1,4 +1,41 @@
-# Source this in your ~/.zshrc
+#     __                      __  ___
+#    / /   ____ _____  __  __/ / / (_)____
+#   / /   / __ `/_  / / / / / /_/ / / ___/
+#  / /___/ /_/ / / /_/ /_/ / __  / (__  )
+# /_____/\__,_/ /___/\__, /_/ /_/_/____/
+#                   /____/
+#
+# init.zsh - Shell initialization for lazyhis history integration
+#
+# Add the following to the end of your ~/.zshrc:
+#     eval "$(lazyhis init zsh)"
+#
+# Inspired by atuin: https://github.com/atuinsh/atuin/blob/main/crates/atuin/src/shell/atuin.zsh
+#
+# ---
+#
+# MIT License (source: https://github.com/atuinsh/atuin/blob/main/LICENSE)
+#
+# Copyright (c) 2021 Ellie Huxtable
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 autoload -U add-zsh-hook
 
 zmodload zsh/datetime 2>/dev/null
